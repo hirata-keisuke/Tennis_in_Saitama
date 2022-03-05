cc70fe6d6aec8bba42bfdf3bb2e0cdaa4dde67aa
hirata-keisuke <plainpeace39th@gmail.com>
2022-03-05 12:47:35 +0900

    mod func,test

--- src/func.py
+++ src/func.py
@@ -1,12 +1,13 @@
 import datetime as dt
-import jpholiday
+import jpholiday, calendar
 import re, unicodedata
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from dateutil.relativedelta import relativedelta
 from time import sleep
 
-def go_to_top_page(browser, url="https://www.pa-reserve.jp/eap-rj/rsv_rj/core_i/init.asp?KLCD=119999&SBT=1&Target=_Top&LCD="):
+def go_to_top(browser, url="https://www.pa-reserve.jp/eap-rj/rsv_rj/core_i/init.asp?KLCD=119999&SBT=1&Target=_Top&LCD="):
     """
     埼玉県県営公園の施設予約サイトに遷移する
 
@@ -18,7 +19,7 @@ def go_to_top_page(browser, url="https://www.pa-reserve.jp/eap-rj/rsv_rj/core_i/
     url : str
         遷移先のURL
     """
-    # 指定したURLに遷移させる
+    # 指定したURLに遷移する
     browser.get(url=url)
 
     # サイトがframeタグで囲まれているので、frameに移動する
@@ -27,7 +28,8 @@ def go_to_top_page(browser, url="https://www.pa-reserve.jp/eap-rj/rsv_rj/core_i/
 
 def login(browser, id="", pwd=""):
     """
-    一度、マイメニューに入ってログインする。後で抽選予約するときに、1回目の予約だけログイン情報を聞かれるのを避ける。
+    一度、マイメニューに入ってログインする。
+    coockieを先に取得して、後で抽選予約するときに、1回目の予約だけログイン情報を聞かれるのを避ける。
 
     Parameters
     ==========
@@ -41,6 +43,72 @@ def login(browser, id="", pwd=""):
         利用者パスワード
     """
 
+    # マイメニューへ遷移する
+    button = browser.find_element(by=By.XPATH, value="//input[@alt='マイメニュー']")
+    button.click()
+
+    # 利用者IDを入力する項目を探し出して、入力する
+    id_input = browser.find_element(by=By.NAME, value="txt_usr_cd")
+    id_input.send_keys(id)
+
+    # パスワードを入力する項目を探し出して、入力する
+    pwd_input = browser.find_element(by=By.NAME, value="txt_pass")
+    pwd_input.send_keys(pwd)
+
+    # OKボタンを押下する
+    button = browser.find_element(by=By.NAME, value="btn_ok")
+    button.click()
+
+    # ログインできたので再びメニューに戻る
+    button = browser.find_element(by=By.XPATH, value="//input[@alt='メニューへ戻る']")
+    button.click()
+
+def get_other_competitors(browser):
+    """
+    大宮第二公園の施設抽選・予約ページに遷移して、抽選を既に申請している人数を、日ごと・コートごと・時間帯ごとで取り出す。
+
+    Parameters
+    ==========
+    brawser : selenium.webdriver.chrome.webdriver.WebDriver
+        seleniumで起動したChromeのブラウザドライバ
+
+    Returns
+    =======
+    competitors : dict (of dict (of dict))
+        日ごと・コートごと・時間帯ごとの申請者をまとめた辞書
+    """
+
+    # 所在地選択ページへ遷移する
+    button = browser.find_element(by=By.XPATH, value="//input[@alt='所在地']")
+    button.click()
+
+    # 中央エリアを選択する
+    browser.execute_script("javascript:onclick=cmdSelect_click('00001', '中央エリア');")
+    button = browser.find_element(by=By.XPATH, value="//input[@alt='検索実行']")
+    button.click()
+
+    # 大宮第二公園の抽選ページに遷移する
+    browser.execute_script("cmdLotYoyaku_click('000050', '00001');return false;")
+
+    # 抽選したい、翌月のカレンダーに切り替える
+    _today = dt.date.today()
+    next_month = _today + relativedelta(months=1)
+    next_month = next_month.replace(day=1)
+    browser.execute_script(
+        f"javascript:set_cal_cal({next_month.strftime('%Y%m%d')}, '00000000', '99999999')"
+    )
+
+    # 既に申し込んでいる人数の表示ON
+    browser.find_element(by=By.ID, value="radio01").click()
+
+    # 土日祝日を抽選申し込み対象にする
+    holidays = list()
+    for days in range(calendar.monthrange(year=next_month.year, month=next_month.month)[1]):
+        date = next_month + dt.timedelta(days=days)
+        if date.weekday() >= 5 or jpholiday.is_holiday(date):
+            holidays.append(date)
+        
+    print(holidays)
 
 if __name__ == "__main__":
 
--- src/test.py
+++ src/test.py
@@ -1,4 +1,17 @@
+from time import sleep
+import func
 
+from selenium import webdriver
 
 if __name__ == "__main__":
-    ...
+    browser = webdriver.Chrome()
+
+    func.go_to_top(browser=browser)
+
+    func.login(browser=browser, id="070805keih", pwd="0785")
+
+    try:
+        func.get_other_competitors(browser=browser)
+        sleep(3)
+    finally:
+        browser.close()
